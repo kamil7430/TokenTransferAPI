b6e2de3488e8902217a3603209205a95413b3293
Kamil Błażejczyk <01189072@pw.edu.pl>
2026-01-03 18:47:06 +0100

    Rewrite repository and implement database repository

--- repository/database_wallet_repository.go
+++ repository/database_wallet_repository.go
@@ -2,25 +2,40 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/kamil7430/TokenTransferAPI/graph/model"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type DatabaseWalletRepository struct {
 }
 
-func (d DatabaseWalletRepository) GetWalletByAddress(ctx context.Context, tx *gorm.DB, address string) (*model.Wallet, error) {
-	//TODO implement me
-	panic("implement me")
+func (d *DatabaseWalletRepository) GetWalletByAddress(ctx context.Context, tx *gorm.DB, address string) (*model.Wallet, error) {
+	wallet, err := gorm.G[model.Wallet](tx).Where("Address = ?", address).First(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &wallet, nil
 }
 
-func (d DatabaseWalletRepository) GetWalletByAddressForUpdate(ctx context.Context, tx *gorm.DB, address string) (*model.Wallet, error) {
-	//TODO implement me
-	panic("implement me")
+func (d *DatabaseWalletRepository) GetWalletByAddressForUpdate(ctx context.Context, tx *gorm.DB, address string) (*model.Wallet, error) {
+	wallet, err := gorm.G[model.Wallet](tx, clause.Locking{Strength: "UPDATE"}).Where("Address = ?", address).First(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &wallet, nil
 }
 
-func (d DatabaseWalletRepository) UpdateWalletByAddress(ctx context.Context, tx *gorm.DB, address string, wallet *model.Wallet) error {
-	//TODO implement me
-	panic("implement me")
+func (d *DatabaseWalletRepository) UpdateWalletTokensByAddress(ctx context.Context, tx *gorm.DB, address string, tokens string) error {
+	rows, err := gorm.G[model.Wallet](tx).Where("Address = ?", address).Update(ctx, "Tokens", tokens)
+	if err != nil {
+		return err
+	}
+	if rows != 1 {
+		return errors.New(fmt.Sprintf("affected %d rows, expected 1", rows))
+	}
+	return nil
 }
